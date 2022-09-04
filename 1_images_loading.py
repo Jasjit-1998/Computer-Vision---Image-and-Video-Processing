8ebccb5d2a0461e7149c23e432c16642e72c817e
Jasjit-1998 <singh.jasjit1998@gmail.com>
2022-09-04 01:39:27 -0400

    Displaying Image on Desktop

--- 1_images_loading.py
+++ 1_images_loading.py
@@ -1,6 +1,18 @@
+# Opencv converts the image into the numpy array
+
 import cv2
 
 # PAth of the image
 # Reading Color imiga = 1, black and white = 0
 img = cv2.imread("galaxy.jpg",1)
 print(type(img))
+print(img.shape)
+
+# Displaying the image on the window
+# <SYNTAX> cv2.imshow("<Windows name>", <imgage variable>)
+cv2.imshow("Galaxy", img)
+# Waitkey 
+# 0 --> if user press any button it will close the window
+# Or we can also add the timme
+cv2.waitKey(2000)
+cv2.destroyAllWindows()
