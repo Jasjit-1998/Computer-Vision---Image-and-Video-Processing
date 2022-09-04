8da6f2f94b610afcf3afa115083b12673149538e
Jasjit-1998 <singh.jasjit1998@gmail.com>
2022-09-04 01:40:49 -0400

    On pressing button image will close

--- 1_images_loading.py
+++ 1_images_loading.py
@@ -11,8 +11,8 @@ print(img.shape)
 # Displaying the image on the window
 # <SYNTAX> cv2.imshow("<Windows name>", <imgage variable>)
 cv2.imshow("Galaxy", img)
-# Waitkey 
+# Waitkey
 # 0 --> if user press any button it will close the window
 # Or we can also add the timme
-cv2.waitKey(2000)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
