12792faf19c4337f36898b4339177b11717c2c10
Jasjit-1998 <singh.jasjit1998@gmail.com>
2022-09-04 03:37:09 -0400

    Grey Scale Version of Image

--- Capturing Video/capturing_video.py
+++ Capturing Video/capturing_video.py
@@ -14,8 +14,11 @@ check, frame = video.read()
 print(check)
 print(frame)
 
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
 time.sleep(3)    # Holding the script for 3 seconds
-cv2.imshow("Capturing", frame)
+cv2.imshow("Capturing", gray)
+
 
 cv2.waitKey(0)
 video.release()
