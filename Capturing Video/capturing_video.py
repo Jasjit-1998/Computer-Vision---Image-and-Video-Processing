fb5c0f180de04617d45affecf66462d9a1ed2c04
Jasjit-1998 <singh.jasjit1998@gmail.com>
2022-09-04 03:33:50 -0400

    Camera capturing photo

--- Capturing Video/capturing_video.py
+++ Capturing Video/capturing_video.py
@@ -1,9 +1,25 @@
-import cv2
+import cv2, time
 
 # Videos are just seris of images
 # We will be reading images/frames one by one
 
 # Trigger Video Capture Object to capture the video from the webcamp
 # In the below code camera will immidiately turn on and off
+# So we will be using the time Library
+
+
 video = cv2.VideoCapture(0)
+
+check, frame = video.read()
+print(check)
+print(frame)
+
+time.sleep(3)    # Holding the script for 3 seconds
+cv2.imshow("Capturing", frame)
+
+cv2.waitKey(0)
 video.release()
+cv2.detroyAllWindows()
+
+
+# Creating the frame object which will read video of that Object
